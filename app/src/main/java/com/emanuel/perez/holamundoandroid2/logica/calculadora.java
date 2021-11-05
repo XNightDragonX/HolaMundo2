6af5d08fe874f424dfab14edb8b57d92fa6d95a4
XNightDragonX <XNightDragonX@outlook.com>
2021-11-05 09:26:55 -0600

    cambio en logica de calculadora

---
+++ app/src/main/java/com/emanuel/perez/holamundoandroid2/logica/calculadora.java
@@ -0,0 +1,23 @@
+package com.emanuel.perez.holamundoandroid2.logica;
+
+import java.io.Serializable;
+
+public class calculadora implements Serializable {
+    public void suma()
+    {
+
+    }
+    public void resta()
+    {
+
+    }
+    public void multiplicacion()
+    {
+
+    }
+    public void division()
+    {
+
+    }
+
+}
